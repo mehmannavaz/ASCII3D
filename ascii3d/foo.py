1288cd5141d68605480d31099cf8251b31b98460
mehmannavaz <mohammadmahanmehmannavaz@gmail.com>
2025-12-22 05:30:17 +0330

    refactor: clean up

--- ascii3d/foo.py
+++ ascii3d/foo.py
@@ -5,50 +5,10 @@ class ascii:
         self.art = art
         self.length = art.count('\n')
         self.width = max([len(line) for line in art.split('\n')])
-        # Turned
-        self.turned_art = ""
-        self.turned_length = self.length + 1 
-        self.turned_width = self.width + 5
-
-        self.matrix = np.zeros((self.turned_length, self.turned_width))
+        self.matrix = np.zeros((self.turned_length, self.turned_width)) # Temporal
 
     def turn_left_down(self):
         pass
-Example2 = """
-         __         
-       _|  |_       
-     _|      |_     
-    |  _    _  |    
-    | |_|  |_| |    
- _  |  _    _  |  _ 
-|_|_|_| |__| |_|_|_|
-  |_|_        _|_|  
-    |_|      |_|    
-"""
-
-Example2 = """
- __________
-|  __  __  |
-|_|0 ||0 |_|
-| |__||__| |
-|   ____   |
-|__________|
-"""
-
-Example2 = """
- __________
-|  __  __  |
-| |  ||  | |
-| |__||__| |
-|          |
-|__________|
-"""
-Example2 = """
- ______ 
-| _  _ |
-||_||_||
-|______|
-"""
 
 # Head (Top)
 counter = 1
